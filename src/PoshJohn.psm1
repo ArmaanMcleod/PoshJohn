bd7f72617737819b1a0c9b90b963acbf824d0bd0
ArmaanMcleod <armaan_mcleod@outlook.com>
2025-12-13 19:43:56 +1100

    chmod the binaries in run folder for mac/linux

--- src/PoshJohn.psm1
+++ src/PoshJohn.psm1
@@ -1,2 +1,20 @@
 $dllPath = Join-Path -Path $PSScriptRoot -ChildPath 'PoshJohn.dll'
 Import-Module $dllPath
+
+# Set execute permissions on all files in the run directory (Linux/macOS only)
+if ($IsLinux) {
+    $runDir = Join-Path $PSScriptRoot 'john/linux/run'
+    if (Test-Path $runDir) {
+        Get-ChildItem $runDir | ForEach-Object {
+            & chmod +x $_.FullName
+        }
+    }
+}
+if ($IsMacOS) {
+    $runDir = Join-Path $PSScriptRoot 'john/macos/run'
+    if (Test-Path $runDir) {
+        Get-ChildItem $runDir | ForEach-Object {
+            & chmod +x $_.FullName
+        }
+    }
+}
