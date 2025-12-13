981b3003db5ac5ffeb7809cc184c1a0a0e330d79
ArmaanMcleod <armaan_mcleod@outlook.com>
2025-12-13 12:01:06 +1100

    multi-OS build

--- scripts/build-john-linux.sh
+++ scripts/build-john-linux.sh
@@ -25,18 +25,6 @@ else
   echo "All required packages are already installed."
 fi
 
-# Install PowerShell if not present
-if ! command -v pwsh &> /dev/null; then
-  echo "Installing PowerShell..."
-  wget -q "https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
-  dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
-  apt-get update
-  apt-get install -y powershell
-else
-  echo "PowerShell is already installed."
-fi
-
 JOHN_REPO="https://github.com/openwall/john.git"
 JOHN_DIR="$(dirname "$0")/../john/linux"
 SRC_DIR="$JOHN_DIR/src"
