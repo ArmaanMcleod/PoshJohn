0e8bcb190139684e6041e4d8ca469bb22942a698
ArmaanMcleod <armaan_mcleod@outlook.com>
2025-12-19 00:07:03 +1100

    Add shared library build steps to make file

--- src/pdf2john/pdfhash.h
+++ src/pdf2john/pdfhash.h
@@ -1,6 +1,29 @@
 #ifndef PDFHASH_H
 #define PDFHASH_H
 
-char *get_pdf_hash(const char *path);
-
+#ifdef _WIN32
+#ifdef PDFHASH_EXPORTS
+// When building the DLL
+#define PDFHASH_API __declspec(dllexport)
+#else
+// When consuming the DLL
+#define PDFHASH_API __declspec(dllimport)
 #endif
+#else
+// Linux/macOS: default visibility
+#define PDFHASH_API __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    // Exported function
+    PDFHASH_API char *get_pdf_hash(const char *path);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PDFHASH_H */
