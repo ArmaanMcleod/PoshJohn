a53ffc00531d5f216581583c8afe2fbe44867d27
ArmaanMcleod <armaan_mcleod@outlook.com>
2026-01-18 17:28:43 +1100

    Update linux docker file with new install deps script

---
+++ scripts/install-deps-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+PKG_FILE="$SCRIPT_DIR/packages-linux.txt"
+if [ ! -f "$PKG_FILE" ]; then
+    echo "Package file $PKG_FILE not found!"
+    exit 1
+fi
+
+echo "Checking and installing required system dependencies from $PKG_FILE..."
+apt-get install -y $(cat "$PKG_FILE")
